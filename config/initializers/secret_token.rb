42b173f066b2b3def4553df21df9ca91820172d8
Gaurav <gauravsoni.ror@gmail.com>
2014-10-13 14:50:11 +0530

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindreader::Application.config.secret_token = '01464d199e96223c23615169b081b19bc6d9f5f116d3af518a2a9d36cc8b93651c288fc2f38a34b1f5605a1faee48c4bd4a7bc855b60c4ae7240e0ddad4c8e34'
